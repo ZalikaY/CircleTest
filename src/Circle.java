de889de7746b6c0bc201d28a5ff01ea2efb0226e
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-28 14:32:54 -0400

    Best Edit to date, add doc and validation

--- src/Circle.java
+++ src/Circle.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 public class Circle {
 
 	//Instantiate Instant variable
+	public static int objectCount=0;
 	public double Circumference;
 	public String FormattedCircumference;
 	public double Area;
@@ -12,6 +13,7 @@ public class Circle {
 	//Constructor 
 	public Circle(double ra) {
 		radius = ra;
+		objectCount++;
 	}
 	//Set methods
 	public void setRadius(double ra){
@@ -34,9 +36,27 @@ public class Circle {
 		return 2 * Math.PI * radius;
 	}
 	
+	public String getFormattedCircumference() {
+		double cirF = getCircumference() ;
+		return String.format("%.2f", cirF);	
+	}
+
+	public String getFormattedArea() {
+		double areF = getArea() ;
+		return String.format("%.2f" , areF);
+	}
+	
+		
 	
 
 
-
+//Static because it's shared with all instances
+public static int getObjectCount(){
+	return objectCount;
+}
+	
 
 }
+
+
+
--- src/CircleApp.java
+++ src/CircleApp.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 
 public class CircleApp {
 
-	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String cont = "y";
+		
+		while(cont.equalsIgnoreCase("y")) {
+			
+		System.out.println("Welcome to the circle tester");
+		
+		
+		System.out.println("Enter radius: ");
+		double ra=sc.nextDouble();
+		Circle cir1 = new Circle(ra);
+		
+		System.out.println("Circumference: " + cir1.getFormattedCircumference());
+		System.out.println("Area: " + cir1.getFormattedArea());
+		System.out.println("Continue? (y/n)");
+		cont = sc.next();
+		
+		
+		}
+		System.out.println("Goodbye.You created " + Circle.objectCount + "Circle objects(s)." );
+		
+		
+	}
+
 }
